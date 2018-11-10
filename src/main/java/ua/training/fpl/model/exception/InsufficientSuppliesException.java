785f0d1c7a2c2d88140878cccbef35ff84f2eb92
javaphite <javaphite@gmail.com>
2018-11-10 18:09:58 +0200

    Add custom exception: InsufficientSuppliesException

---
+++ src/main/java/ua/training/fpl/model/exception/InsufficientSuppliesException.java
@@ -0,0 +1,6 @@
+package ua.training.fpl.model.exception;
+
+// TODO: implement body after service and controller layers ready
+public class InsufficientSuppliesException extends RuntimeException {
+
+}
