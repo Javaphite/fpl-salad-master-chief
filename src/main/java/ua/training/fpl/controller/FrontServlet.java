b7a035603d795fda063d2e5e9871279b5c503668
javaphite <javaphite@gmail.com>
2018-11-12 00:11:55 +0200

    Add FrontServlet

---
+++ src/main/java/ua/training/fpl/controller/FrontServlet.java
@@ -0,0 +1,40 @@
+package ua.training.fpl.controller;
+
+import ua.training.fpl.Configuration;
+import ua.training.fpl.command.DisplayKnownRecipes;
+import ua.training.fpl.command.DoFallback;
+import ua.training.fpl.command.HttpServletCommand;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("")
+public class FrontServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resolveCommand(req).execute(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resolveCommand(req).execute(req, resp);
+    }
+
+    private HttpServletCommand resolveCommand(HttpServletRequest req) {
+        Class<? extends HttpServletCommand> commandClazz =
+                Configuration.getCommandMapping().get(req.getParameter("action"));
+        try {
+            return commandClazz.getConstructor().newInstance();
+        } catch (Exception exception) {
+            // TODO: log and fallback
+            return new DoFallback();
+        }
+    }
+}
